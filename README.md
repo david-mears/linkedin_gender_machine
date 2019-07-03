3c320bcd70c74f0bce88ecd87dd02e2fb2318eb6
David Mears <kieukmail@gmail.com>
2019-07-03 01:29:52 +0100

    Working as intended

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A command-line-interface app that guesses at the gender-proportions of companies who have LinkedIn profiles. Caveat: you can't tell someone's gender from their name, for a variety of reasons. So take the data with a pinch of salt.
 
+The library I'm using for guessing genders provides a probability estimate, so for example 'James' returns 0.99 probability of being male. If the company had only one employee, named James, LinkedIn Gender Machine would estimate that 0.99 employees were male, and 0.01 'other'.
+
 ## Installation
 
 Create and activate the virtual environment. Then:
--- script.py
+++ script.py
@@ -1,11 +1,13 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 import os
 import sys
 import time
 
+from dotenv import load_dotenv
+from genderize import Genderize
 from selenium import webdriver
+
+print('Starting...')
+load_dotenv()
 driver = webdriver.Firefox()
 
 # Sign in to LinkedIn and navigate to the desired company's personnel
@@ -19,11 +21,29 @@ password = driver.find_element_by_id('password')
 password.send_keys(os.getenv("LINKEDIN_PASSWORD"))
 password.submit()
 url = sys.argv[1]
-time.sleep(3)
-driver.get(url + '/people')
+company_name_list = url.split('/')[4].split('-')
+company_name = []
+for word in company_name_list: company_name.append(word.capitalize())
+company_name = ' '.join(company_name)
 
-# Scroll down the whole page
+time.sleep(3)
+if url[-1] != '/': url += '/'
+driver.get(url + 'people/')
+
+print('Successfully hacked into the main-frame.')
+
+# Get LinkedIn's own count of employees
+number_string = driver.find_element_by_class_name('t-20').text.split(' ')[0]
+linkedin_employee_count = int(''.join(number_string.split(',')))
+
 SCROLL_PAUSE_TIME = 2
+
+MAX_EMPLOYEE_COUNT = 200
+if linkedin_employee_count > MAX_EMPLOYEE_COUNT:
+    print('Sorry: there are ' + str(linkedin_employee_count) + ' employees at this company.')
+    print('To load all of them would take me ' + str((SCROLL_PAUSE_TIME * linkedin_employee_count)/60).split('.')[0] + ' minutes.')
+    print('I will sample the first ' + str(MAX_EMPLOYEE_COUNT) + '-ish employees only.')
+# Scroll down the whole page
 # Get scroll height
 last_height = driver.execute_script("return document.body.scrollHeight")
 while True:
@@ -39,6 +59,12 @@ while True:
         break
     last_height = new_height
 
+    # Break if enough employees found
+    employees_found = len(driver.find_elements_by_class_name('org-people-profile-card__profile-title'))
+    print('Employees found: ' + str(employees_found))
+    if employees_found > MAX_EMPLOYEE_COUNT:
+        break
+
 # Scrape names
 name_cards = driver.find_elements_by_class_name('org-people-profile-card__profile-title')
 names = []
@@ -46,8 +72,35 @@ for card in name_cards:
     first_name = card.text.split()[0]
     names.append(first_name)
 
-linkedin_employee_count = driver.find_element_by_class_name('t-20').text
+# Close webdriver
 driver.close()
 
-print('Number of names found: ' + str(len(names)))
-print('Number of names according to LinkedIn: "' + linkedin_employee_count + '"')
+# Tally genders
+gender_tally_dict = {}
+total = 0
+for person in Genderize().get(names):
+    try:
+        probability = person['probability']
+    except:
+        probability = 1.0 # i.e. We are certain that Genderizer hasn't labelled it male or female
+    try:
+        gender_tally_dict[person['gender']] += probability
+    except:
+        gender_tally_dict[person['gender']] = probability
+    total += probability
+gender_percent_dict = {}
+for gender in gender_tally_dict:
+    gender_percent_dict['gender'] = 100 * (gender_tally_dict['gender']/total)
+
+# Report
+print('\n\n')
+title = company_name + ' Report'
+underline = ''
+for char in title: underline += '-'
+print(title)
+print(underline)
+print('  Number of names found:                  ' + str(len(names)))
+print('  Number of names according to LinkedIn: ' + str(linkedin_employee_count))
+print('Gender tally:')
+for key in gender_tally_dict:
+    print('  ' + key.capitalize() + ': ' + gender_tally_dict[key] + ' (' + gender_percent_dict[key] + '%)')
